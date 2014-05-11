f6749063cc90e7a36d770e1087a6e6316b9b9743
rmt/src <144435+rmtsrc@users.noreply.github.com>
2014-05-11 16:43:53 +0100

    Added French Republican calendar implementation

--- app/scripts/lib/calendar.js
+++ app/scripts/lib/calendar.js
@@ -1,6 +1,6 @@
 /*global define*/
 
-define([], function() {
+define([], function () {
     'use strict';
 
     /**
@@ -12,5 +12,115 @@ define([], function() {
         var year = this.getFullYear();
 
         return ((year % 4 === 0) && (year % 100 !== 0)) || (year % 400 === 0);
+    };
+
+    /**
+     * Gets the number of days since the French Republican calendar epoch
+     *
+     * @see http://www.windhorst.org/calendar/
+     * @returns {number}
+     */
+    Date.prototype.getDaysSinceFrcEpoch = function () {
+        var frcEpoch = new Date(1792, 9, 21),
+            dateDiff = this.getTime() - frcEpoch.getTime();
+
+        return Math.round(dateDiff / (24 * 60 * 60 * 1000));
+    };
+
+    /**
+     * Returns the date as a French Republican calendar date
+     *
+     * @returns {{year: number, month: number, day: number}}
+     */
+    Date.prototype.getFrcDate = function () {
+        var totalDays = this.getDaysSinceFrcEpoch(),
+            dayCount = 1,
+            revDay = 0,
+            revMonth = 1,
+            revYear = 1,
+            leap = false;
+
+        // "while" loop counts through the difference in days, applying the rules of the French calendar
+        // and "accumulating" the French date
+        while (dayCount <= totalDays) {
+            revDay = revDay + 1;
+            if (revDay === 31) {
+                revDay = 1;
+                revMonth = revMonth + 1;
+            }
+            if (revMonth === 13) {
+                leap = false;
+                if (revYear === 3 || revYear === 7 || revYear === 11 || revYear === 15) { //4 "if" statements apply the Romme rule for French leap years
+                    leap = true;
+                }
+                if (revYear >= 20 && revYear % 4 === 0) {
+                    leap = true;
+                }
+                if (revYear >= 100 && revYear % 100 === 0) {
+                    leap = false;
+                }
+                if (revYear >= 400 && revYear % 400 === 0) {
+                    leap = true;
+                }
+                if (!leap) {
+                    if (revDay > 5) {
+                        revDay = 1;
+                        revMonth = 1;
+                        revYear = revYear + 1;
+                    }
+                }
+                if (leap) {
+                    if (revDay > 6) {
+                        revDay = 1;
+                        revMonth = 1;
+                        revYear = revYear + 1;
+                    }
+                }
+            }
+            dayCount = dayCount + 1;
+        }
+
+        return {
+            'year': revYear,
+            'month': revMonth,
+            'day': revDay
+        };
+    };
+
+    /**
+     * Gets the French Republican calendar year
+     *
+     * @returns {number}
+     */
+    Date.prototype.getFrcYear = function () {
+      return this.getFrcDate()['year'];
+    };
+
+
+    /**
+     * Gets the French Republican calendar quarter
+     *
+     * @returns {number}
+     */
+    Date.prototype.getFrcQuarter = function () {
+        return Math.floor(this.getFrcMonth() / 3) + 1;
+    };
+
+    /**
+     * Gets the French Republican calendar month
+     *
+     * @returns {number}
+     */
+    Date.prototype.getFrcMonth = function () {
+        return this.getFrcDate()['month'];
+    };
+
+    /**
+     * Gets the French Republican calendar day
+     *
+     * @returns {number}
+     */
+    Date.prototype.getFrcDay = function () {
+        return this.getFrcDate()['day'];
     };
 });
--- test/spec/lib/calendarSpec.js
+++ test/spec/lib/calendarSpec.js
@@ -22,6 +22,45 @@ define([
                 date = new Date(2020, 5, 10, 12, 0, 0, 0);
                 expect(date.isLeapYear()).toBeTruthy();
             });
+
+            describe('French Republican', function () {
+                it('should get the correct number of days since FR epoch', function () {
+                    expect(date.getDaysSinceFrcEpoch()).toBe(80950);
+                });
+
+                it('should compute the correct FR date', function () {
+                    expect(date.getFrcDate()).toEqual({
+                        'year': 222,
+                        'month': 8,
+                        'day': 22
+                    });
+                });
+
+                it('should get the correct year', function () {
+                    expect(date.getFrcYear()).toBe(222);
+                });
+
+                it('should get the correct quarter', function () {
+                    expect(date.getFrcQuarter()).toBe(3);
+
+                    date = new Date(2014, 9, 24, 0, 0, 0, 0);
+                    expect(date.getFrcQuarter()).toBe(1);
+
+                    date = new Date(2014, 12, 23, 0, 0, 0, 0);
+                    expect(date.getFrcQuarter()).toBe(2);
+
+                    date = new Date(2014, 6, 20, 0, 0, 0, 0);
+                    expect(date.getFrcQuarter()).toBe(4);
+                });
+
+                it('should get the correct month', function () {
+                    expect(date.getFrcMonth()).toBe(8);
+                });
+
+                it('should get the correct day', function () {
+                    expect(date.getFrcDay()).toBe(22);
+                });
+            });
         });
     });
 });
