9b9d8ad476d24988370a37c5a62df2cf347f44c6
rmt/src <144435+rmtsrc@users.noreply.github.com>
2017-06-04 16:31:42 +0100

    Added initial failing test

--- test/flattenTree.js
+++ test/flattenTree.js
@@ -1,3 +1,12 @@
 const test = require('ava');
+const flattenTree = require('../lib/flattenTree');
 
-test.todo('flattens correctly the provided examples');
+const questionset = require('../examples/questionset.json');
+const answers = require('../examples/answers.json');
+const expectedOutput = require('../examples/expectedOutput.json');
+
+test('flattens correctly the provided examples', t => {
+  const actual = flattenTree(questionset, answers);
+
+  t.deepEqual(actual, expectedOutput);
+});
